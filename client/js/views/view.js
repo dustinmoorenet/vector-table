4bb66a073672787d9f57b030e3f894fc671a732e
Dustin Moore <dustin@ordoro.com>
2015-03-30 09:15:32 -0500

    Removed hammerJS!!!

--- client/js/views/view.js
+++ client/js/views/view.js
@@ -1,12 +1,3 @@
 var AmpersandView = require('ampersand-view');
-var events = require('../libs/events');
 
-module.exports = AmpersandView.extend({
-    _handleElementChange: function (element, delegate) {
-        if (this.eventManager) { this.eventManager.unbind(); }
-        this.eventManager = events(this.el, this);
-        this.delegateEvents();
-        this._applyBindingsForKey();
-        return this;
-    },
-});
+module.exports = AmpersandView.extend({});
